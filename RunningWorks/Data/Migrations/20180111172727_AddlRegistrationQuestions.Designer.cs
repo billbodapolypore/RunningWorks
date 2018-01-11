b44b3cc3f954d0f3286da4eb0a269af94387906d
bill <bill.boda@polypore.net>
2018-01-11 12:28:10 -0500

    Adding additional questions migration

--- RunningWorks/Data/Migrations/20180111172727_AddlRegistrationQuestions.Designer.cs
+++ RunningWorks/Data/Migrations/20180111172727_AddlRegistrationQuestions.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace RunningWorks.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180111172727_AddlRegistrationQuestions")]
+    partial class AddlRegistrationQuestions
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RunningWorks/Data/Migrations/20180111172727_AddlRegistrationQuestions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace RunningWorks.Data.Migrations
+{
+    public partial class AddlRegistrationQuestions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsHomeless",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IsHomeless",
+                table: "AspNetUsers");
+        }
+    }
+}
